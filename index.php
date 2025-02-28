f1587816c551991b1a547a82becb0010eb1e052d
laopan25 <lissaalisa25@gmail.com>
2025-02-28 20:04:57 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,366 @@
+
+
+
+
+
+
+	<!DOCTYPE html>
+	<html amp lang="id">
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="format-detection" content="telephone=no">
+		<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1">
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<script async src="https://cdn.ampproject.org/v0.js"></script>
+		<link rel="canonical" href="https://konsentrasi.upstegal.ac.id/image/">
+		<link rel="icon" type="image/x-icon" href="https://imagizer.imageshack.com/img924/4084/d8BJ1v.png">
+		<title>Scatter Hitam & Scatter Mahjong X500 Fitur Terbaru Pg Soft Slot Mahjong</title>
+		<meta name="keywords" content="rajawali888"/>
+		<meta name="title" content="Scatter Hitam & Scatter Mahjong X500 Fitur Terbaru Pg Soft Slot Mahjong">
+		<meta name="description" content="Kini provider pg soft menghadirkan inovasi terbaru yaitu scatter hitam dan juga mahjong x500 dalam permain slot mahjong ways menjadikan game tersebut paling populer ditahun ini.">
+		<style amp-boilerplate>
+			body {
+				-webkit-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+				-moz-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+				-ms-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+				animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+			}
+
+			@-webkit-keyframes -amp-start {
+				from {
+					visibility: hidden
+				}
+
+				to {
+					visibility: visible
+				}
+			}
+
+			@-moz-keyframes -amp-start {
+				from {
+					visibility: hidden
+				}
+
+				to {
+					visibility: visible
+				}
+			}
+
+			@-ms-keyframes -amp-start {
+				from {
+					visibility: hidden
+				}
+
+				to {
+					visibility: visible
+				}
+			}
+
+			@-o-keyframes -amp-start {
+				from {
+					visibility: hidden
+				}
+
+				to {
+					visibility: visible
+				}
+			}
+
+			@keyframes -amp-start {
+				from {
+					visibility: hidden
+				}
+
+				to {
+					visibility: visible
+				}
+			}
+		</style>
+		<noscript>
+			<style amp-boilerplate>
+				body {
+					-webkit-animation: none;
+					-moz-animation: none;
+					-ms-animation: none;
+					animation: none;
+				}
+			</style>
+		</noscript>
+		<style amp-custom>
+			.atasbox,
+			.center,
+			.footer {
+				text-align: center
+			}
+
+			* {
+				box-sizing: border-box;
+				margin: 0;
+				padding: 0
+			}
+
+			:focus {
+				outline: 0
+			}
+
+			::-webkit-scrollbar {
+				display: none
+			}
+
+			a,
+			a:after,
+			a:hover,
+			a:visited {
+				text-decoration: none;
+				color: #dfdfdf;
+			}
+
+			html {
+				max-width: 500px;
+				margin: 0 auto;
+				background: url(https://imagedelivery.net/tqEyS-vlTMhKtRbTaDoEfg/01750dbb-3a1a-4b08-5cd5-a262db12d200/public) no-repeat fixed;
+		background-size: 100% 100%;
+			}
+
+			body {
+				color: #dfdfdf;
+				font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+			}
+
+			h2 {
+				margin: 20px;
+				padding: 10px;
+				background-color: #1f1f1f;
+				border-bottom: 2px solid transparent;
+				background-image: linear-gradient(#313131, #000), radial-gradient(circle at left top, #2420ff, #100791);
+				background-origin: border-box;
+				background-clip: padding-box, border-box;
+				border-radius: 10px;
+			}
+
+			button {
+				margin: 5px 5px 50px 5px;
+				width: 50%; height: 40px; 
+				background-image: linear-gradient(#494949, #2e2e2e); 
+				color: #b6b6b6;
+				font-size: 0.8em; 
+				border-radius: 10px;
+				font-weight: bold;
+				transition: 0.5s all;
+			}
+
+			button:hover {
+				border-bottom: 2px solid transparent;
+				background-image: linear-gradient(#313131, #000), radial-gradient(circle at left top, #2420ff, #100791);
+				background-origin: border-box;
+				background-clip: padding-box, border-box;
+				color: #fff;
+			}
+
+			.atas {
+				display: grid;
+				min-height: 85vh;
+			}
+
+			.atasbox {
+				margin: auto;
+			}
+
+			.ataslink {
+				display: inline-grid;
+				margin: 1rem;
+			}
+
+			.ataslink a {
+				margin: 0 0 20px 0;
+				color: #fff;
+				position: relative;
+				font-size: 0.8em;
+				font-weight: bold;
+				text-transform: uppercase;
+				padding: 16px 35px;
+				border-radius: 25px;
+				overflow: hidden;
+				border: 3px double transparent;
+				background-image: linear-gradient(#313131, #000), radial-gradient(circle at left top, #ffd620, #918f07);
+				background-origin: border-box;
+				background-clip: padding-box, border-box;
+				transition: 0.5s all;
+			}
+
+			.logo-kiri {
+				position: absolute;
+				left: 0;
+				top: 50%;
+				width: 50px;
+				transform: translateY(-50%);
+			}
+
+			.logo-kanan {
+				position: absolute;
+				right: 0;
+				top: 50%;
+				width: 50px;
+				transform: translateY(-50%);
+			}
+
+			.ataslink a:hover {
+				color: #1f1f1f;
+				background-image: linear-gradient(#2420ff, #100791), radial-gradient(circle at left top, #ffd620, #918f07)
+			}
+
+			.ataslink a:hover span {
+				cursor: pointer;
+				display: inline-block;
+				position: relative;
+				transition: .2s;
+			}
+
+			.ataslink span:after {
+				content: '\00bb';
+				position: absolute;
+				opacity: 0;
+				top: 0;
+				right: -20px;
+				transition: .2s;
+			}
+
+			.debar {
+				animation: 1.5s ease 0s infinite beat;
+				margin: 0px 25px 0 25px;
+			}
+
+			@keyframes beat {
+
+				0%,
+				50%,
+				100% {
+					transform: scale(1, 1);
+				}
+
+				30%,
+				80% {
+					transform: scale(0.92, 0.95);
+				}
+			}
+
+			@keyframes pulse {
+
+				0%,
+				50%,
+				100% {
+					background: #fee;
+				}
+
+				30%,
+				80% {
+					background: #fff;
+				}
+			}
+
+			.klikajah:hover span {
+				padding-right: 20px;
+				transition: .2s
+			}
+
+			.klikajah:hover span:after {
+				opacity: 1;
+				right: 0
+			}
+
+			.center {
+				margin-bottom: 10px
+			}
+
+			.imghero {
+				border-radius: 8px;
+				border: 2px solid #fff;
+				margin: 1rem;
+			}
+
+			.demo {
+				display: grid;
+				grid-template-columns: 1fr 1fr 1fr;
+				grid-gap: 5px;
+				overflow: hidden;
+				margin: 20px;
+
+			}
+		
+			.demo-gim {
+				padding: 15px;
+				text-align: center;
+				border: 2px double transparent;
+				background-image: linear-gradient(#313131, #000), radial-gradient(circle at left top, #ffd620, #918f07);
+				background-origin: border-box;
+				background-clip: padding-box, border-box;
+				transition: 0.5s all;
+			}
+
+			.demo-gim:hover {
+				color: #1f1f1f;
+				background-image: linear-gradient(#100791 20%, #000 80%), radial-gradient(circle at left top, #ffd620, #918f07);
+				background-origin: border-box;
+				background-clip: padding-box, border-box;
+			}
+		  
+			.nama-gim {
+				font-size: 0.6em;
+				font-weight: bold;
+				padding: 5px;
+				margin: 10px 0 0 0;
+			}
+
+			.demo-gim amp-img {
+				border-radius: 10px;
+			}
+
+			.footer {
+				position: absolute;
+				left: 0;
+				bottom: 50;
+				width: 100%;
+				font-size: 0.7em;
+			}
+		</style>
+	</head>
+	<body>
+		<main>
+			<div class="atas">
+				<div class="atasbox">
+					<div>
+						<amp-img class="imghero" height="700" width="700" layout="responsive" alt="SLOT ONLINE"
+							src="https://imagizer.imageshack.com/img923/9807/CAJhL0.jpg"></amp-img>
+					</div>
+					<div class="ataslink">
+						<a href="http://seobasah.com/login/rajawali/" class="klikajah" rel="nofollow">
+							<img src="https://imagedelivery.net/tqEyS-vlTMhKtRbTaDoEfg/72e53fa8-b300-4c2a-5560-3ea3ce46e800/public" class="logo-kiri">
+							<div class="debar">Daftar Slot Online</div>
+							<img src="https://imagedelivery.net/tqEyS-vlTMhKtRbTaDoEfg/72e53fa8-b300-4c2a-5560-3ea3ce46e800/public" class="logo-kanan">
+						</a>
+						<a href="http://seobasah.com/login/rajawali/" class="klikajah" rel="nofollow">
+							<img src="https://imagedelivery.net/tqEyS-vlTMhKtRbTaDoEfg/72e53fa8-b300-4c2a-5560-3ea3ce46e800/public" class="logo-kiri">
+							<div class="debar">Login Slot Online</div>
+							<img src="https://imagedelivery.net/tqEyS-vlTMhKtRbTaDoEfg/72e53fa8-b300-4c2a-5560-3ea3ce46e800/public" class="logo-kanan">
+						</a>
+						<a href="http://seobasah.com/login/rajawali/" class="klikajah" rel="nofollow">
+							<img src="https://imagedelivery.net/tqEyS-vlTMhKtRbTaDoEfg/c0cf45b2-dc6d-4517-d76f-1e4b89797b00/public" class="logo-kiri">
+							<div class="debar">LIVE CHAT</div>
+							<img src="https://imagedelivery.net/tqEyS-vlTMhKtRbTaDoEfg/c0cf45b2-dc6d-4517-d76f-1e4b89797b00/public" class="logo-kanan">
+						</a>
+					</div>
+					<a href="http://seobasah.com/login/rajawali/" rel="nofollow">
+						<button>RTP SLOT ONLINE SLOT »</button>
+					</a>
+					<div class="footer">
+						<p>&copy;2025 | Scatter Hitam & Scatter Mahjong X500 Fitur Terbaru Pg Soft Slot Mahjong</p>
+						<p>SLOT ONLINE</p>
+					</div>
+				</div>
+			</div>
+		</main>
+	</body>
+
+	</html>
